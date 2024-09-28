3478def7f0cf5d7b8c6d10a6c38dcbf4c678d952
XVVQAQ <XVVQAQ143352@outlook.com>
2024-09-28 22:12:25 +0800

    changes

--- kubejs/startup_scripts/newBlocks/register.js
+++ kubejs/startup_scripts/newBlocks/register.js
@@ -4,7 +4,6 @@ StartupEvents.registry("block", (event) => {
     .hardness(1.0) // Set hardness (affects mining time)
     .resistance(1.0) // Set resistance (to explosions, etc)
     .requiresTool(true) // Requires a tool or it won't drop (see tags below)
-    .tagBlock("my_namespace:my_other_tag") // Tag the block with `#my_namespace:my_other_tag`
     .tagBlock("mineable/axe") //can be mined faster with an axe
     .tagBlock("mineable/pickaxe") // or a pickaxe
     .tagBlock('minecraft:needs_iron_tool') // the tool tier must be at least iron
