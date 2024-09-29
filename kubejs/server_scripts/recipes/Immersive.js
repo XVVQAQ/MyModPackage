879b998b0bb3e667bc39812d4ba406a5955fd60a
Suirent <547858853@qq.com>
2024-09-29 16:33:13 +0800

    任务书第一章+部分魔改

---
+++ kubejs/server_scripts/recipes/Immersive.js
@@ -0,0 +1,6 @@
+ServerEvents.recipes(event => {
+    //替换工程师锤子
+    event.replaceInput({mod:'immersiveengineering',output:'#immersiveengineering:tools/hammers'},'minecraft:iron_ingot' , '#forge:storage_blocks/iron')
+    //焦炉块配方更改
+    //高炉配方更改
+})
