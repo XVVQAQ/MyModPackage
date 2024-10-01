cf083bf00ee471980ac12da646c139c11b7b1105
XVVQAQ <XVVQAQ143352@outlook.com>
2024-10-01 16:29:30 +0800

    Fix Item Proble

--- kubejs/startup_scripts/newItems/registerItem.js
+++ kubejs/startup_scripts/newItems/registerItem.js
@@ -3,6 +3,6 @@ StartupEvents.registry('item',(event)=> {
     event.create('flint_axe_head').texture('kubejs:item/flint_axe_head')
     event.create('flint_shovel_head').texture('')
     event.create('flint_pickaxe_head')
-    event.create('golden_string').texture('kubejs:kubejs/golden_string')
-    event.create('source_ingot').texture('kubejs:kubejs/source_ingot')
+    event.create('golden_string').texture('kubejs:item/golden_string')
+    event.create('source_ingot').texture('kubejs:item/source_ingot')
   })
